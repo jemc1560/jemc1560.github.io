9906e5b0c5078bcf2b0c18144d29e22f28627bf0
jade <msfemjem@gmail.com>
2025-01-03 16:17:18 -0500

    webiste :3

---
+++ app.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  const elements = document.querySelectorAll('.p1');
+  elements.forEach((el) => observer.observe(el));
+});
