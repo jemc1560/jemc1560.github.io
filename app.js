3fdd67d49beec1645e4c827971358982617764e0
jade <msfemjem@gmail.com>
2025-01-13 17:10:04 -0500

    fixed overlap in js

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll('.p1');
   elements.forEach((el) => observer.observe(el));
 });
+
+function checkOverlap() {
+  const element1 = document.getElementById('header-wrapper');
+  const element2 = document.getElementById('contain');
+
+  if (isOverlapping(element1, element2)) {
+    element2.style.opacity = '50%';
+  } else {
+    element2.style.opacity = '100%';
+  }
+}
+function isOverlapping(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+
+  return !(
+      rect1.bottom < rect2.top ||
+      rect1.top > rect2.bottom
+  );
+}
+
+window.addEventListener('resize', checkOverlap);
+window.addEventListener('scroll', checkOverlap);
